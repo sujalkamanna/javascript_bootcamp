7b44069dc0989253bbce36bc70f667974a025cd1
Sujal Kamanna <sujalkamanna2003@gmail.com>
2025-06-26 01:26:01 +0530

    update test.js

--- week1/js/day1/test.js
+++ week1/js/day1/test.js
@@ -1,14 +1,1 @@
-// Import the readline module
-const readline = require('readline');
-
-// Create interface for input/output
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-// Ask the user for input
-rl.question('What is your name? ', (name) => {
-  console.log(`Hello, ${name}!`);
-  rl.close(); // Close the input stream
-});
+console.log("hello world");
---
+++ week1/js/day1/test2.js
@@ -0,0 +1,4 @@
+console.log("hello")
+
+let age = prompt("enter your age :");
+console.log(age);
