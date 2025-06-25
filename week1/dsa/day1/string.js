00d4a68f32309dbd864d7667a73d4c16b5fdba10
Sujal Kamanna <sujalkamanna2003@gmail.com>
2025-06-26 00:41:37 +0530

    added dsa day1

---
+++ week1/dsa/day1/string.js
@@ -0,0 +1,7 @@
+let str1 = "Hello, world!";
+let str2 = 'Single quotes also work.';
+let str3 = `Template literals let you embed values like ${2 + 2}`;
+
+console.log(str1);
+console.log(str2);
+console.log(str3);
