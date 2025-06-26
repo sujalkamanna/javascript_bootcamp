762e5358526098d882519f16fac8d678f5141dcb
Sujal Kamanna <sujalkamanna2003@gmail.com>
2025-06-27 01:19:57 +0530

    added2 files

---
+++ week1/javascript/day2/if_else.js
@@ -0,0 +1,8 @@
+let a = 10;
+
+if (a == 10){
+    console.log("The value of a is : ",a);
+}
+else{
+    console.log("The value of a is not 10 ");
+}
---
+++ week1/javascript/day2/if_elseif_else.js
@@ -0,0 +1,14 @@
+let a = 30;
+
+if (a == 20){
+    console.log("The value of a is : ",a);
+}
+else if(a<20){
+    console.log("The value of a less than 20");
+}
+else if(a>20){
+    console.log("The value of a is greater than 20");
+}
+else{
+    console.log("Error");
+}
