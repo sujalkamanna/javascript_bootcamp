e56fd236e12126f3443cc3fee3c451f03539fc7a
Sujal Kamanna <sujalkamanna2003@gmail.com>
2025-06-27 21:01:43 +0530

    added string.js file

--- week1/dsa/day1/string.js
+++ week1/dsa/day1/string.js
@@ -1,7 +1,26 @@
 let str1 = "Hello, world!";
-let str2 = 'Single quotes also work.';
-let str3 = `Template literals let you embed values like ${2 + 2}`;
+let str2 = "Single quotes also work.";
 
 console.log(str1);
 console.log(str2);
-console.log(str3);
+
+console.log(str1.length);
+console.log(str2.length);
+
+let len = str1.length;
+console.log(len);
+
+console.log(str1[1]);
+console.log(str2[0]);
+
+comb = str1 + " (added space) " + str2;
+console.log(comb);
+
+lw_case = str1.toLowerCase();
+console.log(lw_case)
+
+lw_case = str2.toUpperCase(); 
+console.log(lw_case)
+
+charat = "This is string";
+console.log(charat.charAt(0))
