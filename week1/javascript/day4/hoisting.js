92682020fddefda5914a9e5ef1309116ae18f02f
Sujal Kamanna <sujalkamanna2003@gmail.com>
2025-06-29 01:28:57 +0530

    updated hoisting.js

--- week1/javascript/day4/hoisting.js
+++ week1/javascript/day4/hoisting.js
@@ -1,13 +1,25 @@
 // value hoisting
 
-console.log(a) //undefined
+console.log(a); //undefined
 var a = 10;
 
 // function hoisting
 
-add(10,20) //output = 30
-function add(a,b){ 
-    console.log(a+b)
+add(10, 20); //output = 30
+function add(a, b) {
+  console.log(a + b);
 }
 
 //hoisting for function expressions
+
+// hoisting_in_fe()
+var hoisting_in_fe = function () {
+  console.log("inside function");
+};
+hoisting_in_fe();
+
+// console.log(b) !! ReferenceError
+let b = 20;
+
+// console.log(c) !! ReferenceError 
+const c = 30;
