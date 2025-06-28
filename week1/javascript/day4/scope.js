051174a3e4bb05eafac405f87433198346911742
Sujal Kamanna <sujalkamanna2003@gmail.com>
2025-06-29 01:43:34 +0530

    added scope.js and updated

---
+++ week1/javascript/day4/scope.js
@@ -0,0 +1,27 @@
+//function scope
+
+function call_x() {
+  var x = 10;
+  console.log(x);
+}
+call_x();
+
+// console.log(x) ReferenceError
+
+if (true) {
+  let b = 20;
+  const c = 30;
+  console.log(b, c);
+}
+// console.log(b, c); //ReferenceError
+
+//nested funtions - lexical scoping
+
+function outer() {
+  let x = 1000;
+  function inner() {
+    console.log(x);
+  }
+  inner();
+}
+outer();
