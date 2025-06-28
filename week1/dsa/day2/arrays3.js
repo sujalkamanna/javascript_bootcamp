858f2c1b7da32f287ed797b787ac67fb2c41726c
Sujal Kamanna <sujalkamanna2003@gmail.com>
2025-06-28 20:00:42 +0530

    feat: create arrays3.js

---
+++ week1/dsa/day2/arrays3.js
@@ -0,0 +1,13 @@
+// array methods
+
+arr = [10, 54, 84, 96, 56, 23, 54];
+
+let str = arr.toString();
+console.log(str);
+console.log(typeof str);
+
+let c = arr.join("_");
+console.log(c);
+
+arr.pop();
+console.log(arr);
