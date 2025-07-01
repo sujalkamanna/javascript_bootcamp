b82f99dfaab880d3957cbf3840b8ccb7473b1085
Sujal Kamanna <sujalkamanna2003@gmail.com>
2025-07-01 17:50:00 +0530

    added for each

--- javascript/week1/day6/array_methods.js
+++ javascript/week1/day6/array_methods.js
@@ -2,9 +2,23 @@ function a() {
   function b() {
     console.log(10);
     let c = 200;
-}
-b();
-console.log(c)
+  }
+  b();
+  // console.log(c)
 }
 a();
 // console.log(c)
+
+//for each
+
+arr1 = [1, 2, 3, 4, 5, 6];
+
+arr1.forEach((element) => {
+  console.log(element);
+});
+
+let nums = [42, 51, 89, 41];
+console.log(nums);
+nums.forEach((element) => {
+  console.log(element);
+});
