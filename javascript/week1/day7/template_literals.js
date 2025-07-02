0e38760b8ba689d078fb321999c10f1d9fcb06ac
Sujal Kamanna <sujalkamanna2003@gmail.com>
2025-07-03 00:16:35 +0530

    update template_literals.js

--- javascript/week1/day7/template_literals.js
+++ javascript/week1/day7/template_literals.js
@@ -3,19 +3,25 @@ let age = 10;
 let city = "LA";
 
 final = `hello my name is, ${name} and im ${age} years old and i live in ${city}`;
-console.log(final)
+console.log(final);
 
-console.log(`hello my name is, ${name} and im ${age} years old and i live in ${city}`)
+console.log(
+  `hello my name is, ${name} and im ${age} years old and i live in ${city}`
+);
 
-console.log('--------------------\n')
+console.log("--------------------\n");
 
 let num1 = 7;
 let num2 = 8;
 
-let result = num1+num2
-console.log("The addition of 7 and 8 is:",result)
+let result = num1 + num2;
+console.log("The addition of 7 and 8 is:", result);
 console.log(`The addition of ${num1} and ${num2} is: ${result}
-`)
+`);
 
-console.log("the addition of ",num1,"and",num2,"is:",result )
-console.log("the addition of ",num1,"and",num2,"is:"+result )
+console.log("the addition of ", num1, "and", num2, "is:", result);
+console.log("the addition of ", num1, "and", num2, "is:" + result);
+
+console.log("my name is \nxyz");
+console.log(`My name is
+xyz`);
