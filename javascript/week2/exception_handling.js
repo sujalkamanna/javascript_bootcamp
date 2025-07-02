5c242a3fb61e10342cc8f91cced47b4292d60427
Sujal Kamanna <sujalkamanna2003@gmail.com>
2025-07-03 00:52:22 +0530

    added exception handling

---
+++ javascript/week2/exception_handling.js
@@ -0,0 +1,28 @@
+// errors - user defined
+
+console.log("Hello");
+
+console.log("You have reached the end of the program");
+
+try {
+  console.log(x);
+} catch {
+  console.error("error!! x is not declared");
+} finally {
+  console.log("solve the error"); // this always exeecutes
+}
+
+const prompt = require("prompt-sync")();
+
+const value = parseInt(prompt("Please enter a no:"));
+console.log(typeof value);
+
+try {
+  if (isNaN(value)) {
+    throw new Error("Error :invalid value");
+  }
+}
+catch (error) {
+  console.error("please enter a numeric value");
+  console.error(error.message)
+}
