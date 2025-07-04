2a706f639470a4059ea284417de00ecf5d262899
Sujal Kamanna <sujalkamanna2003@gmail.com>
2025-07-04 23:59:25 +0530

    added await and sync

--- javascript/week2/day2/async_await.js
+++ javascript/week2/day2/async_await.js
@@ -2,3 +2,27 @@
 // async makes a function return a promise
 //await makes an async function wait for a promise
 
+async function function_name() {
+  let delhi = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("27 C");
+    }, 1000);
+  });
+
+  let banglore = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("pleasent");
+    }, 2000);
+  });
+  //   delhi.then((temp) => {
+  //     console.log("the weather of delhi is :", temp);
+  //   });
+  let delhiW = await delhi;
+  let bangloreW = await banglore;
+  return [delhiW, bangloreW];
+  //   banglore.then((weather) => {
+  //     console.log("the weather of banglore is :", weather);
+  //   });
+}
+console.log("welcome to weather control room");
+function_name();
