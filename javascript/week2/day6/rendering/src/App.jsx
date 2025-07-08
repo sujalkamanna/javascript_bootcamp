ddd7042a1949c6e4b866dccc652d13a2b7aed557
Sujal Kamanna <sujalkamanna2003@gmail.com>
2025-07-09 01:47:57 +0530

    update App.jsx

--- javascript/week2/day6/rendering/src/App.jsx
+++ javascript/week2/day6/rendering/src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  
+  const [count, setcount] = useState(0);
+  const [showbtn, setshowBtn] = useState(true);
+
+  return (
+    <div>
+      {/* {showbtn?<button> I will be shown only when second button is clicked</button>:"nothing"} */}
+      {showbtn && (
+        <button onClick={() => setshowBtn(!showbtn)}>
+          I will be shown only when second button is clicked
+        </button>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
