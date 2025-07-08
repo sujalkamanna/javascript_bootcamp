18a726dfe67fff8f41a078c476b29c9894e8149d
Sujal Kamanna <sujalkamanna2003@gmail.com>
2025-07-09 01:27:39 +0530

    update App.jsx, main.jsx and javascript/week2/day5/props/README.md

--- javascript/week2/day5/hook_state/src/App.jsx
+++ javascript/week2/day5/hook_state/src/App.jsx
@@ -1,8 +1,23 @@
-import { useState } from 'react'
-import './App.css'
+import { useState,useEffect } from "react";
+import "./App.css";
 
 function App() {
-    
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    alert("hello")
+  }, [])
+
+  useEffect(() => {
+    alert("count was changed")
+  }, [count])
+  
+  return (
+    <div>
+      <p>the count is {count}</p>
+      <button onClick={() => setCount(count + 1)}>Update count</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
