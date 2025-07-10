1bde6ceb0dcf3cb563a34b17e1bebbec5447c2f1
Sujal Kamanna <sujalkamanna2003@gmail.com>
2025-07-10 17:36:18 +0530

    update App.css, App.jsx and javascript/week2/day7/event_handling/src/index.css

--- javascript/week2/day7/event_handling/src/App.jsx
+++ javascript/week2/day7/event_handling/src/App.jsx
@@ -1,7 +1,43 @@
-import { useState } from 'react'
-
+import { useState } from "react";
+import "./App.css";
 function App() {
+  const [count, setCount] = useState(0);
+  // const [name, setName] = useState("harry");
+  const [form, setForm] = useState({ email: "", phone: "" });
+  const handleClick = () => {
+    alert("hey I'm clicked");
+  };
 
+  const handleMouseOver = () => {
+    alert("I'm a mouse hover");
+  };
+  const handleChange = (e) => { //e = event
+    // setName(e.target.value);
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  return (
+    <>
+      <div className="button">
+        <button onClick={handleClick}> ClickMe</button>
+      </div>
+
+      <div className="red">
+        <p>I'm a red div</p>
+      </div>
+      <input
+        type="text"
+        name="email"
+        value={form.name}
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="phone"
+        value={form.phone}
+        onChange={handleChange}
+      />
+    </>
+  );
 }
 
-export default App
+export default App;
