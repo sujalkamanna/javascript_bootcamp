1580357c1591458150c11af6a9ff34d10af3b9e3
Sujal Kamanna <sujalkamanna2003@gmail.com>
2025-07-14 01:43:07 +0530

    update javascript/week3/corecomp/app/(tabs)/index.tsx

--- javascript/week3/corecomp/app/(tabs)/index.tsx
+++ javascript/week3/corecomp/app/(tabs)/index.tsx
@@ -1,4 +1,6 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, ImageBackground } from "react-native";
+
+const local_image = require("../../assets/images/adaptive-icon.png");
 
 export default function App() {
   return (
@@ -21,6 +23,7 @@ export default function App() {
       >
         <Text style={{ color: "white" }}>this is a text</Text>
       </View>
+
       <View
         style={{
           width: 200,
@@ -32,9 +35,29 @@ export default function App() {
         }}
       >
         <Text>
-          this is a<Text style={{ color: "white" }}> white text</Text>
+          this is a <Text style={{ color: "white" }}>white text</Text>
         </Text>
       </View>
+
+      <Image source={local_image} style={{ margin: 10, height: 100, width: 100 }} />
+
+      <Image
+        source={{ uri: "https://picsum.photos/seed/picsum/200/300" }}
+        style={{ margin: 10, height: 300, width: 300 }}
+      />
+
+      <ImageBackground
+        source={local_image}
+        style={{
+          width: 200,
+          height: 200,
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 20,
+        }}
+      >
+        <Text style={{ color: "white", fontWeight: "bold" }}>IMAGE TEXT</Text>
+      </ImageBackground>
     </View>
   );
 }
