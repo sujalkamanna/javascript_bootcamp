fe75999dc871ca26afdf4cb003608181a535a711
Sujal Kamanna <sujalkamanna2003@gmail.com>
2025-07-14 17:33:11 +0530

    update javascript/week3/butn_n_more/app/(tabs)/index.tsx and javascript/week3/corecomp/app/(tabs)/index.tsx

--- javascript/week3/butn_n_more/app/(tabs)/index.tsx
+++ javascript/week3/butn_n_more/app/(tabs)/index.tsx
@@ -1,75 +1,46 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  ImageBackground,
+  ScrollView,
+  StyleSheet,
+} from "react-native";
+import { styles1 } from "./styles";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function HomeScreen() {
+const local_image = require("../../assets/images/adaptive-icon.png");
+export default function App() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.title}>this is title</Text>
+        <Image source={local_image} style={styles.img} />
+        <Text style={styles1.styling}>
+          this is a text with padding 16 and margin 10 imported from external
+          file
+        </Text>
+        <Text style={{ backgroundColor: "blue", margin: 10, color: "plum" }}>
+          this is some random text and added styling using inline css
+        </Text>
+      </View>
+    </ScrollView>
   );
 }
-
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    fontWeight: "100",
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  img: {
+    width: 200,
+    height: 200,
+    borderColor: "black",
+    borderWidth: 1,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
---
+++ javascript/week3/butn_n_more/app/(tabs)/styles.tsx
@@ -0,0 +1,7 @@
+import { StyleSheet } from "react-native";
+export const styles1 = StyleSheet.create({
+  styling: {
+    padding: 16,
+    margin:10,
+  },
+});
