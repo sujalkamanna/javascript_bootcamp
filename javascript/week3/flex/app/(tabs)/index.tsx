88678eb49a1b9acabc16a9bea1961079c198e9e4
Sujal Kamanna <sujalkamanna2003@gmail.com>
2025-07-22 06:10:28 +0000

    Jul 22, 2025, 11:40 AM

--- javascript/week3/flex/app/(tabs)/index.tsx
+++ javascript/week3/flex/app/(tabs)/index.tsx
@@ -1,75 +1,46 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text } from "react-native";
+import Box from "@/components/box";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+// export default function HomeScreen() {
+//   return (
+//     <View>
+//       <View style={styles.container}>
+//         <View>
+//           <Text>flex box</Text>
+//         </View>
+//         <Box style={{ backgroundColor: "red" }}>Box1</Box>
+//         <Box style={{ backgroundColor: "#e0b9b9ff" }}>Box2</Box>
+//         <Box style={{ backgroundColor: "#fff000" }}>Box3</Box>
+//         {/* <Box style={{ backgroundColor: "#312d2dff" }}>Box4</Box>
+//         <Box style={{ backgroundColor: "plum" }}>Box5</Box> */}
+//       </View>
+//       {/* <View style = {{backgroundColor:"plum",flex:1}}>
 
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+//       </View> */}
+//     </View>
+//   );
+// }
+
+export default function HomeScreen(){
+  return(
+    <View>
+      <View>
+        <Text>
+          input
+        </Text>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  container: {
+    flex: 1,
+    justifyContent:"flex-end",
+    // flexDirection:"row",
+    // marginTop: 50,
+    borderWidth: 5,
+    borderColor: "red",
+    alignItems:"flex-start",
   },
 });
---
+++ javascript/week3/flex/components/box.tsx
@@ -0,0 +1,33 @@
+import { View, Text, StyleSheet ,ViewStyle, StyleProp } from "react-native";
+import React, { ReactNode } from "react";
+
+interface BoxProps {
+  children: ReactNode;
+  style?: StyleProp<ViewStyle>;
+}
+
+export default function Box({ children, style }: BoxProps) {
+  return (
+    <View>
+      <View style={[styles.box, style]}>
+        <Text style={styles.text}>{children}</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  box: {
+    backgroundColor: "white",
+    padding: 20,
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+    color:"white"
+  },
+});
