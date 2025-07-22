2599f749723d3e0b284034b87b1c7126650df724
Sujal Kamanna <sujalkamanna2003@gmail.com>
2025-07-22 12:02:20 +0530

    update javascript/week3/flex/app/(tabs)/index.tsx

--- javascript/week3/flex/app/(tabs)/index.tsx
+++ javascript/week3/flex/app/(tabs)/index.tsx
@@ -1,37 +1,26 @@
 import { View, StyleSheet, Text } from "react-native";
+import { TextInput } from "react-native";
 import Box from "@/components/box";
 
-// export default function HomeScreen() {
-//   return (
-//     <View>
-//       <View style={styles.container}>
-//         <View>
-//           <Text>flex box</Text>
-//         </View>
-//         <Box style={{ backgroundColor: "red" }}>Box1</Box>
-//         <Box style={{ backgroundColor: "#e0b9b9ff" }}>Box2</Box>
-//         <Box style={{ backgroundColor: "#fff000" }}>Box3</Box>
-//         {/* <Box style={{ backgroundColor: "#312d2dff" }}>Box4</Box>
-//         <Box style={{ backgroundColor: "plum" }}>Box5</Box> */}
-//       </View>
-//       {/* <View style = {{backgroundColor:"plum",flex:1}}>
+export default function HomeScreen() {
+  return (
+    <View>
+      <View style={styles.container}>
+        <View>
+          <Text>flex box</Text>
+        </View>
+        <Box style={{ backgroundColor: "red" }}>Box1</Box>
+        <Box style={{ backgroundColor: "#e0b9b9ff" }}>Box2</Box>
+        <Box style={{ backgroundColor: "#fff000" }}>Box3</Box>
+        {/* <Box style={{ backgroundColor: "#312d2dff" }}>Box4</Box>
+        <Box style={{ backgroundColor: "plum" }}>Box5</Box> */}
+      </View>
+      {/* <View style = {{backgroundColor:"plum",flex:1}}>
 
-//       </View> */}
-//     </View>
-//   );
-// }
-
-// export default function HomeScreen(){
-//   return(
-//     <View>
-//       <View>
-//         <Text>
-//           input
-//         </Text>
-//       </View>
-//     </View>
-//   );
-// }
+      </View> */}
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
