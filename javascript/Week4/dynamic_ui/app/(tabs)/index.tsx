eb6f83d580bae794c20170dcebc3e32ccfb4d9fc
Sujal Kamanna <sujalkamanna2003@gmail.com>
2025-07-23 01:32:06 +0530

    update javascript/Week4/dynamic_ui/app/(tabs)/index.tsx

--- javascript/Week4/dynamic_ui/app/(tabs)/index.tsx
+++ javascript/Week4/dynamic_ui/app/(tabs)/index.tsx
@@ -1,13 +1,32 @@
-import { Platform,View, StyleSheet } from 'react-native';
-
+import { Platform, View, StyleSheet } from "react-native";
+import { Text, Dimensions } from "react-native";
 export default function HomeScreen() {
   return (
-    <View>
-      
+    <View style={styles.container}>
+      <View style={styles.box}>
+        <Text style={styles.text}>Welcome!</Text>
+      </View>
     </View>
   );
 }
+const window_width = Dimensions.get("window").width;
+const window_height = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    backgroundColor: "plum",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  box: {
+    width: window_width > 500 ? "70%" : "90%",
+    height: window_width > 600 ? "60%" : "90%",
+    backgroundColor: "lightblue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: window_width > 500 ? 50 : 24,
+  },
 });
