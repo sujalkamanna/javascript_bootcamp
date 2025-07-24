7ea1105912a9d7970352da4941132e888df0a19e
Sujal Kamanna <sujalkamanna2003@gmail.com>
2025-07-25 02:40:28 +0530

    update javascript/Week4/form_input/app/(tabs)/index.tsx

--- javascript/Week4/form_input/app/(tabs)/index.tsx
+++ javascript/Week4/form_input/app/(tabs)/index.tsx
@@ -1,75 +1,85 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import React from "react";
+import {
+  Platform,
+  StyleSheet,
+  TextInput,
+  View,
+  Text,
+  StatusBar,
+  SafeAreaView,
+  Switch,
+} from "react-native";
+import { useState } from "react";
 
 export default function HomeScreen() {
+  const [name, setname] = useState("");
+  const [isDarkMode, setisDarkMode] = useState(false);
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text>Add data</Text>
+        <TextInput
+          style={styles.text_input}
+          onChangeText={setname}
+          placeholder="email@example.com"
+          autoCorrect={false}
+          autoCapitalize="none"
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+
+        <TextInput
+          style={styles.text_input}
+          placeholder="enter password"
+          secureTextEntry
+          // keyboardType= "numeric"
+        />
+        <Text>My name is {name}</Text>
+
+        <TextInput
+          style={[styles.text_input, styles.multiLineTExt]}
+          placeholder="message"
+          multiline
+        />
+
+        <View style={styles.switchContainer}>
+          <Text style={styles.text}>Dark Mode</Text>
+          <Switch
+            value={isDarkMode}
+            onValueChange={() =>
+              setisDarkMode((previousState) => !previousState)
+            }
+            trackColor={{false:"#000000", true:"#lightblue"}}
+            thumbColor="#f4f33f4"
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  text_input: {
+    height: 40,
+    margin: 12,
+    padding: 10,
+    borderWidth: 1,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  multiLineTExt: {
+    minHeight: 100,
+    textAlignVertical: "top",
+  },
+  switchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 10,
+    marginTop: 20,
+  },
+  text: {
+    fontSize: 16,
   },
 });
