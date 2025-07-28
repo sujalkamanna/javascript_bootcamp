1ab7e129832d956161daeae9897db112e0c0c07a
Sujal Kamanna <sujalkamanna2003@gmail.com>
2025-07-28 16:23:05 +0530

    update javascript/Week4/Navigation/app/(tabs)/index.tsx and HomeScreens.tsx

--- javascript/Week4/Navigation/app/(tabs)/index.tsx
+++ javascript/Week4/Navigation/app/(tabs)/index.tsx
@@ -1,11 +1,16 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Homescreen from "@/screens/HomeScreens";
+const Stack = createNativeStackNavigator();
 
-const Stack = createNativeStackNavigator()
 export default function App() {
-  return <NavigationContainer>{
-    <Stack.Navigator>
-      <Stack.Screen name = "Home"/>
-    </Stack.Navigator>
-}</NavigationContainer>;
+  return (
+    <NavigationContainer>
+      {
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Homescreen}/>
+        </Stack.Navigator>
+      }
+    </NavigationContainer>
+  );
 }
