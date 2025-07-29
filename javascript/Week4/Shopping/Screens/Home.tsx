917e666133beed1a752cbdd0620be8b5af31873f
Sujal Kamanna <sujalkamanna2003@gmail.com>
2025-07-30 01:38:05 +0530

    create 2 files and update 1 file

---
+++ javascript/Week4/Shopping/Screens/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { View ,Text} from 'react-native'
+function HomeScreen() {
+  return (
+    <View><Text> Home page</Text></View>
+  )
+}
+
+export default HomeScreen
