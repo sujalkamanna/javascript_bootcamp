ffbce2d6a3790748c0fbc358446869f2f25ea41d
Sujal Kamanna <sujalkamanna2003@gmail.com>
2025-08-01 01:04:54 +0530

    added new app in week5

---
+++ javascript/week5/style02/app/(tabs)/index.tsx
@@ -0,0 +1,13 @@
+import { Platform, StyleSheet,View,Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+});
